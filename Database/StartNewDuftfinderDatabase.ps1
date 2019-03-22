3300499eccca75fab8cfd3f788e91f869dea17b4
Florian Zimmermann <flozi76@bluewin.ch>
2019-03-22 10:16:52 +0100

    removed database data

--- Database/StartNewDuftfinderDatabase.ps1
+++
@@ -1,5 +0,0 @@
-docker stop duftfinder.db
-docker rm duftfinder.db
-docker network create duftfinderNet
-docker pull flozi76/duftfinder.db
-docker run -d -p 27017:27017 --net duftfinderNet --name duftfinder.db flozi76/duftfinder.db
